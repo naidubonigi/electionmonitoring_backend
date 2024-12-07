d58b6c341910ad590f60cd8167ec795601f83f57
naidubonigi <naidubonigi2005@gmail.com>
2024-12-08 01:01:58 +0530

    second commit

--- src/main/java/com/jfsdsdp/electionmonitoringsystem/config/WebConfig.java
+++ src/main/java/com/jfsdsdp/electionmonitoringsystem/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow all paths
-                .allowedOrigins("http://localhost:3000")  // Allow requests from the frontend
+                .allowedOrigins("https://electionmonitor.netlify.app")  // Allow requests from the frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials
